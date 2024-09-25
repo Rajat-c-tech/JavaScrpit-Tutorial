b4994ab80e444dea7dc2c60fc1da7f2a11bde803
Rajat <rsamal499@gmail.com>
2024-09-26 02:01:53 +0530

    Number Done

---
+++ 01_Basics/06Numbers.js
@@ -0,0 +1,15 @@
+const Score=582;
+
+const Balance=new Number('5698')
+console.log(Balance);
+
+console.log(Balance.toString().length);
+// console.log(typeof(Balance.toString()) );
+console.log(Balance.toFixed(2));
+  
+
+const otherNumber=125.8596;
+console.log(otherNumber.toPrecision(4));
+
+const hundreds=1000000000;
+console.log(hundreds.toLocaleString('en-In'));
